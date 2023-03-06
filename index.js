120f96b9ed51f76e5c4843b5baa52f0bdc215450
SamuelWade <samuelrcwade@gmail.com>
2023-03-06 07:47:49 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Reference
+
+// function changeDay(day){
+//     let h1 = document.querySelector('h1');
+//     h1.textContent = day;
+//     h1.id = 'imAnId';
+//     h1.className = 'className';
+//     h1.style.color='blue'
+// }
+
+
+let newHeader = document.querySelector('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Samuel Wade is the champion';
